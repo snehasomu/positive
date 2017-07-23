f073a3b0978654decb9e49812c77910e678f6e57
snehasomu <snehasomu555@gmail.com>
2017-07-23 16:17:22 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,30 @@
+package aaaaa;
+import java.util.Scanner;
+public class Factorial {
+	int i,n;
+boolean n1;
+	long fact=1;
+	
+	Scanner s=new Scanner(System.in);
+	void fact2()
+	{
+	do {
+			System.out.println("enter the number:");
+		n=s.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			fact=fact*i;
+			}
+			System.out.println("factorial:"+fact);
+			System.out.println("do u want to continue");
+			n1=s.nextBoolean();
+	}while(n1==true);
+}
+		
+	
+public static void main(String[] args) {
+		Factorial f=new Factorial();
+		f.fact2();
+	}
+
+}
